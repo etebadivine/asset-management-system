07deaef9c7d3578ea57b711745c9100e3b8cab12
samuel <samuel@financemobile.app>
2020-08-20 10:44:38 +0000

    Added jpa anotations

--- src/main/java/com/financemobile/fmassets/model/Category.java
+++ src/main/java/com/financemobile/fmassets/model/Category.java
@@ -1,16 +1,28 @@
 package com.financemobile.fmassets.model;
 
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.*;
 import java.util.Date;
 
 @Data
+@Entity
+@Table(name = "category")
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private String description;
+    @Column(name = "created_by")
     private String createdBy;
+    @CreationTimestamp
+    @Column(name = "date_created")
     private Date dateCreated;
+    @UpdateTimestamp
+    @Column(name = "date_modified")
     private Date dateModified;
 }
