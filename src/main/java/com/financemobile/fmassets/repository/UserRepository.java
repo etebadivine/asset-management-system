ec79ff07823c34f0304061091cc6bfc96be6458f
FelixDuedu <felixduedu02@gmail.com>
2020-08-21 14:01:31 -1200

    added user location service repository

---
+++ src/main/java/com/financemobile/fmassets/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.financemobile.fmassets.repository;
+
+import com.financemobile.fmassets.model.User;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface UserRepository extends PagingAndSortingRepository<User,Long>, JpaSpecificationExecutor<User> {
+}
