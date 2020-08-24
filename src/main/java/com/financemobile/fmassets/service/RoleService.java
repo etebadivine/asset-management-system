382f0b09bd9991e8678c61f7c798817a262635b7
FelixDuedu <felixduedu02@gmail.com>
2020-08-24 09:44:45 -1200

    role service test added

--- src/main/java/com/financemobile/fmassets/service/RoleService.java
+++ src/main/java/com/financemobile/fmassets/service/RoleService.java
@@ -4,7 +4,7 @@ import com.financemobile.fmassets.model.Role;
 
 import java.util.List;
 
-public interface RoleService {
+public interface  RoleService {
 
     public Role addRole(String name);
     public Role getRoleById(Long id);
--- src/main/java/com/financemobile/fmassets/service/impl/RoleServiceImpl.java
+++ src/main/java/com/financemobile/fmassets/service/impl/RoleServiceImpl.java
@@ -1,27 +1,47 @@
 package com.financemobile.fmassets.service.impl;
 
+import com.financemobile.fmassets.exception.AlreadyExistException;
+import com.financemobile.fmassets.exception.DataNotFoundException;
 import com.financemobile.fmassets.model.Role;
+import com.financemobile.fmassets.repository.RoleRepository;
 import com.financemobile.fmassets.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoleServiceImpl implements RoleService {
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     @Override
     public Role addRole(String name) {
-        return null;
+        if (roleRepository.existsByName(name))
+            throw new AlreadyExistException("role already exists");
+
+        Role role = new Role();
+        role.setName (name);
+        return roleRepository.save(role);
     }
 
     @Override
-    public Role getRoleById(Long id) {
-        return null;
+    public Role getRoleById(Long id) throws DataNotFoundException {
+        Optional<Role> roleOptional =roleRepository.findById(id);
+
+        if(roleOptional.isPresent()){
+            return roleOptional.get();
+        }
+
+        throw new DataNotFoundException("role not found");
     }
 
     @Override
     public List<Role> getAllRoles() {
-        return null;
+      return roleRepository.findAll();
     }
 
 }
