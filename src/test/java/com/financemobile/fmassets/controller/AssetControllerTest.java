e9e9bba2846657baf73180c79d16ce1b25af5b6c
Reynolds-ak <reynreyn222@gmail.com>
2020-09-08 13:22:32 +0000

    added assign asset method

--- src/test/java/com/financemobile/fmassets/controller/AssetControllerTest.java
+++ src/test/java/com/financemobile/fmassets/controller/AssetControllerTest.java
@@ -289,6 +289,7 @@ public class AssetControllerTest {
 
     @Test
     public void test_assignAsset() throws Exception{
+
         //mock repo and response
         Location location = new Location();
         location.setName("Tema");
